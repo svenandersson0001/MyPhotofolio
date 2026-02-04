bd0b140e05e586e0355d70647a99ea2149021a4e
rodolfo-dev-01 <iamrodolfonava@gmail.com>
2026-02-04 14:20:36 +0000

    fix assets url error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/MyPhotofolio',
+  assetPrefix: '/MyPhotofolio',
   images: {
     domains: [],
     unoptimized: true,
